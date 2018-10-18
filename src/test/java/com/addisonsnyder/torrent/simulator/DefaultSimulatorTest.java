3a5bc294e02d7b809c8e2248f83dc3c1bf15163a
Addison Snyder <addison@addisonsnyder.com>
2018-10-18 18:03:43 -0400

    Added JUnit 5 dependencies and a simple test stub.

---
+++ src/test/java/com/addisonsnyder/torrent/simulator/DefaultSimulatorTest.java
@@ -0,0 +1,19 @@
+package com.addisonsnyder.torrent.simulator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DefaultSimulatorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTickOperations() {
+		fail("Not yet implemented");
+	}
+
+}
